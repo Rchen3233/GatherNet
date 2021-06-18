eb03d3c2a1ee3ff0bbbecf87ef5b645d76b2c0c8
Rchen3233 <23020191153233@stu.xmu.edu.cn>
2021-06-18 19:30:53 +0800

    first

---
+++ main.py
@@ -0,0 +1,183 @@
+'''Train CIFAR10 with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+from tensorboardX import SummaryWriter
+
+import os
+import argparse
+
+from models import *
+from utils import progress_bar
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+    parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--resume', '-r', action='store_true',
+                        help='resume from checkpoint')
+    args = parser.parse_args()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    best_acc = 0  # best test accuracy
+    start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+    # Data
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=0)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=0)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # Model
+    print('==> Building model..')
+    # net = VGG('VGG19')
+    # net = ResNet18()
+    # net = PreActResNet18()
+    # net = GoogLeNet()
+    # net = DenseNet121()
+    # net = ResNeXt29_2x64d()
+    # net = MobileNet()
+    # from models.osdnet_hswish import OsdNet
+    # net = OsdNet(num_classes=10)
+    net = MobileNetV2()
+    # net = DPN92()
+    # net = ShuffleNetG2()
+    # net = SENet18()
+    # net = ShuffleNetV2(1)
+    # net = EfficientNetB0()
+    # net = RegNetX_200MF()
+    # net = SimpleDLA()
+    print(net)
+    net = net.to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+
+    if args.resume:
+        # Load checkpoint.
+        print('==> Resuming from checkpoint..')
+        assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+        checkpoint = torch.load('./checkpoint/ckpt.pth')
+        net.load_state_dict(checkpoint['net'])
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                          momentum=0.9, weight_decay=5e-4)
+    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150],
+    #                                                  gamma=0.1)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+    if not os.path.isdir('mobilev2_model'):
+        os.mkdir('mobilev2_model')
+    with open(os.path.join('./mobilev2_model/results.csv'), 'w') as f:
+        f.write('epoch,train_loss,train_error,valid_loss,valid_error\n')
+
+
+    # Training
+    def train(epoch):
+        print('\nEpoch: %d' % epoch)
+        net.train()
+        train_loss = 0
+        correct = 0
+        total = 0
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(trainloader), 'Losss: %.3f | Accc: %.3f%% (%d/%d)'
+                         % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+            t_loss = (train_loss / (batch_idx + 1))
+            t_error = correct / total
+            return t_loss, t_error
+
+
+    def test(epoch):
+        global best_acc
+        net.eval()
+        test_loss = 0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = net(inputs)
+                loss = criterion(outputs, targets)
+
+                test_loss += loss.item()
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+                progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                             % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+        # Save checkpoint.
+        acc = 100. * correct / total
+        if acc > best_acc:
+            print('Saving..')
+            state = {
+                'net': net.state_dict(),
+                'acc': acc,
+                'epoch': epoch,
+            }
+            torch.save(state, './mobilev2_model/ckpt.pth')
+            best_acc = acc
+        te_loss = (test_loss / (batch_idx + 1))
+        te_error = correct / total
+        return te_loss, te_error , best_acc
+
+    writer = SummaryWriter()
+    for epoch in range(start_epoch, start_epoch + 200):
+        train_loss, train_error = train(epoch)
+        writer.add_scalar('Train_loss', train_loss, epoch)
+        writer.add_scalar('Train_error', train_error, epoch)
+        valid_loss, valid_error, best_acc = test(epoch)
+        writer.add_scalar('Test_loss', valid_loss, epoch)
+        writer.add_scalar('Test_error', valid_error, epoch)
+        with open(os.path.join('./mobilev2_model/results.csv'), 'a') as f:
+            f.write('%03d,%0.6f,%0.6f,%0.5f,%0.5f,\n' % (
+                (epoch + 1),
+                train_loss,
+                train_error,
+                valid_loss,
+                valid_error,
+            ))
+        print(best_acc)
+        scheduler.step()
---
+++ models/mobilenet_v3.py
@@ -0,0 +1,274 @@
+# MobileNet V3 implementation.
+# Paper: https://arxiv.org/pdf/1905.02244.pdf
+
+from typing import Tuple, Union
+import collections
+
+import torch
+from torch import nn
+
+import models.mobilenet_v3_configs as conf
+
+
+def hard_sigmoid(x: torch.Tensor, inplace: bool = True) -> torch.Tensor:
+    return nn.functional.relu6(x + 3, inplace=inplace) / 6
+
+
+def hard_swish(x: torch.Tensor, inplace: bool = True) -> torch.Tensor:
+    return hard_sigmoid(x, inplace=inplace) * x
+
+
+class HardSwish(nn.Module):
+    def __init__(self, inplace: bool = True) -> None:
+        super().__init__()
+        self._inplace = True
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return hard_swish(x, inplace=self._inplace)
+
+
+def _get_activation(activation: str):
+    if activation == "relu":
+        return nn.ReLU
+    elif activation == "relu6":
+        return nn.ReLU6
+    elif activation == "hardswish":
+        return HardSwish
+    else:
+        raise ValueError(f"Unsupported activation: {activation}")
+
+
+# SE and inverted residual are similar to MNASNet, but with MNV3 specific
+# tweaks.
+class _SqueezeAndExcitation(nn.Module):
+    def __init__(self, channels: int, se_ratio: float):
+        if se_ratio <= 0.0:
+            raise ValueError("Squeeze and excitation depth ratio must be positive.")
+        super().__init__()
+        reduced_ch = _round_to_multiple_of(channels * se_ratio, 8)
+        # Note: official implementation uses bias on SE.
+        self.reduce = nn.Conv2d(channels, reduced_ch, 1, bias=True)
+        self.expand = nn.Conv2d(reduced_ch, channels, 1, bias=True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        y = x.mean([2, 3], keepdim=True)
+        y = nn.functional.relu(self.reduce(y), inplace=True)
+        return hard_sigmoid(self.expand(y)) * x
+
+
+class _ConvBnActivationBlock(nn.Module):
+    def __init__(
+        self,
+        in_ch: int,
+        out_ch: int,
+        kernel_size: Union[int, Tuple[int, int]],
+        stride: Union[int, Tuple[int, int]],
+        padding: Union[int, Tuple[int, int, int, int]],
+        dilation: Union[int, Tuple[int, int]],
+        activation: str = "relu",
+    ):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            in_ch,
+            out_ch,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            bias=False,
+        )
+        self.bn = nn.BatchNorm2d(out_ch)
+        self.activation = _get_activation(activation)(inplace=True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        return self.activation(x)
+
+
+class _MobileNetV3Block(nn.Module):
+    def __init__(
+        self,
+        in_ch,
+        exp_ch,
+        out_ch,
+        kernel_size,
+        stride,
+        dilation=1,
+        se_ratio=None,
+        activation="relu",
+        allow_residual=True,
+    ):
+        super().__init__()
+        assert stride in [1, 2]
+        assert kernel_size in [3, 5]
+        activation = _get_activation(activation)
+        self.apply_residual = allow_residual and (in_ch == out_ch and stride == 1)
+        # Features are collected from pointwise immediately before the next
+        # downsampling. If there's no downsampling, we don't keep the features.
+        self.keep_features = stride > 1
+        self.se_ratio = se_ratio
+
+        if in_ch != exp_ch:
+            # Pointwise expand.
+            self.expand = nn.Sequential(
+                nn.Conv2d(in_ch, exp_ch, 1, bias=False),
+                nn.BatchNorm2d(exp_ch),
+                activation(inplace=True),
+            )
+        else:
+            self.expand = None
+
+        effective_kernel_size = (kernel_size - 1) * dilation + 1
+        self.dw_conv = nn.Sequential(
+            nn.Conv2d(
+                exp_ch,
+                exp_ch,
+                kernel_size,
+                padding=effective_kernel_size // 2,
+                stride=stride,
+                dilation=dilation,
+                groups=exp_ch,
+                bias=False,
+            ),
+            nn.BatchNorm2d(exp_ch),
+            activation(inplace=True),
+        )
+
+        if se_ratio is not None:
+            self.se = _SqueezeAndExcitation(exp_ch, se_ratio)
+
+        # Linear pointwise. Note that there's no activation afterwards.
+        self.contract = nn.Sequential(
+            nn.Conv2d(exp_ch, out_ch, 1, bias=False), nn.BatchNorm2d(out_ch)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        y = self.expand(x) if self.expand is not None else x
+        if self.keep_features:
+            self.features = y
+        y = self.dw_conv(y)
+        if self.se_ratio is not None:
+            y = self.se(y)
+        y = self.contract(y)
+        if self.apply_residual:
+            y += x
+        return y
+
+
+def _round_to_multiple_of(val, divisor, round_up_bias=0.9):
+    """ Asymmetric rounding to make `val` divisible by `divisor`. With default
+    bias, will round up, unless the number is no more than 10% greater than the
+    smaller divisible value, i.e. (83, 8) -> 80, but (84, 8) -> 88. """
+    assert 0.0 < round_up_bias < 1.0
+    new_val = max(divisor, int(val + divisor / 2) // divisor * divisor)
+    return new_val if new_val >= round_up_bias * val else new_val + divisor
+
+
+class MobileNetV3(nn.Module):
+    """ MobileNetV3 model: https://arxiv.org/pdf/1905.02244.pdf
+    >>> model = MobileNetV3(alpha=1.5, model_type="small")
+    >>> x = torch.rand(1, 3, 224, 224)
+    >>> y = model.forward(x)
+    >>> list(y.shape)
+    [1, 1000]
+    >>> y.nelement()
+    1000
+    """
+
+    def __init__(
+        self,
+        alpha: float = 1.0,
+        in_ch: int = 3,
+        num_classes: int = 1000,
+        dropout: float = 0.2,  # Per paper.
+        model_type: str = "large",
+        has_classifier: bool = True,
+    ):
+        super().__init__()
+
+        assert alpha > 0.0
+        self.alpha = alpha
+        assert in_ch > 0
+        self.in_ch = in_ch
+        assert num_classes > 1
+        self.num_classes = num_classes
+        assert model_type in conf.CONFIG
+        self.model_type = model_type
+        self.has_classifier = has_classifier
+
+        config = conf.CONFIG[model_type]
+        # Scale the channels, forcing them to be multiples of 8, biased towards
+        # the higher number of channels.
+        for c in config:
+            c[0] = _round_to_multiple_of(c[0] * alpha, 8)
+            c[1] = _round_to_multiple_of(c[1] * alpha, 8)
+            c[2] = _round_to_multiple_of(c[2] * alpha, 8)
+
+        # Build the first layer. It's the same for all networks.
+        self.input_layer = _ConvBnActivationBlock(
+            in_ch,
+            config[0][0],
+            3,  # kernel_size
+            padding=1,
+            stride=1,
+            dilation=1,
+            activation="hardswish",
+        )
+
+        # Build the bottleneck stack.
+        body = collections.OrderedDict()
+        for idx, c in enumerate(config):
+            in_ch, exp_ch, out_ch, kernel_size, stride, dilation, se_ratio, activation = (
+                c
+            )
+            body[f"bottleneck{idx}"] = _MobileNetV3Block(
+                in_ch,
+                exp_ch,
+                out_ch,
+                kernel_size,
+                stride,
+                dilation=dilation,
+                se_ratio=se_ratio,
+                activation=activation,
+            )
+
+        # Build the classifier.
+        shallow_tail = any(x in model_type for x in ["_segmentation", "_detection"])
+        if "large" in model_type:
+            last_conv_ch = 960 if not shallow_tail else 480
+        elif "small" in model_type:
+            last_conv_ch = 576 if not shallow_tail else 288
+        else:
+            raise ValueError("Invalid model type")
+
+        if alpha < 1.0:
+            last_conv_ch = _round_to_multiple_of(last_conv_ch * alpha, 8)
+
+        body["last_conv"] = _ConvBnActivationBlock(
+            config[-1][2],
+            last_conv_ch,
+            1,
+            padding=0,
+            stride=1,
+            dilation=1,
+            activation="hardswish",
+        )
+
+        self.body = nn.Sequential(body)
+
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+            nn.Linear(last_conv_ch, 1280),
+            HardSwish(inplace=True),
+            nn.Dropout(p=dropout, inplace=True),
+            nn.Linear(1280, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.input_layer(x)
+        x = self.body(x)
+        if self.has_classifier:
+            x = self.classifier(x)
+        return x
